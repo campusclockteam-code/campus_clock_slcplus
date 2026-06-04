bbed10c9f95e40a86c98e0cc00a994405c089a2e
CampusClockTeam <campusclock.team@gmail.com>
2026-06-04 14:31:55 +0530

    Migrate to Android Embedding V2 - Complete Android configuration update

---
+++ android/app/src/main/kotlin/com/example/campus_clock_slc/MainApplication.kt
@@ -0,0 +1,18 @@
+﻿package com.example.campus_clock_slc
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainApplication : FlutterApplication(), PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+        if (registry != null) {
+            GeneratedPluginRegistrant.registerWith(registry)
+        }
+    }
+}
